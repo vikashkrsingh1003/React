c72244300866b31d5f86101310e2af9692e9386f
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-02-24 18:27:26 +0530

    24 feb 2025

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,16 +1,22 @@
+ import myimg from './thailand.jpg'
 
 function App() {
   
-
+let name = "chhava"
   return (
-    
+    <>
     <h1> hello everyone </h1>
     <h1> welcome to react class</h1>
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, vero?</p>
    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt eius cumque eveniet ut aperiam magni deserunt, distinctio animi nulla blanditiis minus ipsum dolores quas neque eaque, enim, repellat quisquam numquam. Similique ea vitae sunt enim tempora. Quam ad sint nesciunt possimus labore consequatur velit asperiores nobis maiores aliquid. Non, nisi.</p>
-   <img src="vite.svg" alt="" height="500px" />  
-   )
+   {/* <img src="thailand.jpg" alt="" height="500px" />   */}
+
+        <img src={myimg} alt="" height="500px"/>
+
+        <h1>my name is {name}</h1>
+   </>
+    )
 }
 
 export default App
