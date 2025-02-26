759e30b60d70336f0c8e0f5b9318516c901c19e5
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-02-26 19:03:40 +0530

    26 feb 2025

--- component_Task/src/content.jsx
+++ component_Task/src/content.jsx
@@ -4,11 +4,13 @@ const Content = () => {
     return (
         <>
         <div className="left">
+            <ul>
         <h1> Hello Everybody</h1>
-      
-       
+       <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia enim rerum vero! Beatae ducimus recusandae iste! Commodi aperiam, repellendus nobis explicabo molestias temporibus quia ad id asperiores eum atque eius.</p>
+       </ul>
+       <ul>
         <img src="thailand.jpg" alt="" height="500px"/>
-        
+        </ul>
     </div> 
  
         </>
