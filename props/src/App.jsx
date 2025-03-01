3634147f0dc8dd55d07aa0764fd3f60706df3b77
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-01 17:22:54 +0530

    props

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -5,7 +5,7 @@ import Child1 from './Child1.jsx'
 function App () {
   return (
   <>
-   <Child1 name="Manish Kumar"/>
+   <Child1 name="vikash Singh"/>
  
   </>
 )
---
+++ props/src/Child3.jsx
@@ -0,0 +1,10 @@
+
+function Child3 (age){
+    return (
+        <>
+        <h1>My age is {age.age}</h1>
+        </>
+    )
+}
+
+export default Child3
