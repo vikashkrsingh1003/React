ddc7ace8d71d1c25f5a97f854eb54ad9efedd3f3
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-05 19:05:03 +0530

    5 march

--- states/src/About.jsx
+++ states/src/About.jsx
@@ -7,6 +7,10 @@ function About() {
     })
 
     let [arr, setarr] = useState([1,2,3,4,5])
+
+    let [color, Setcolor] = useState(" ")
+    let [hbd, Sethbd] = useState("HAppy birthday")
+
     return (
         <>
 
@@ -18,6 +22,18 @@ function About() {
             <button onClick={() => setAr({name:"billo", contact:1234})}>Change object </button> <br /><br />
 
             <button onClick={() => setarr([arr[0]=2, arr[1]=99])}>Change arr</button>
+
+            <br /> <br /> <hr />
+  <div style={{height:"100vh", backgroundColor:color}}>
+
+            <button onClick={() => Setcolor('black') }> black</button><br /><br />
+            <button onClick={() => Setcolor('white') }> white </button><br /><br />
+
+            
+            <button onClick={() =>Sethbd("") }> hide</button>
+            <button onClick={() =>Sethbd("HAPPY birthday") }> show</button>
+
+            </div>
         </>
     )
 }
