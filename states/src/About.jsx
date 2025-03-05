07f122e48bf23d01bca7e583429e5453050befef
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-05 18:51:11 +0530

    5 march

--- states/src/About.jsx
+++ states/src/About.jsx
@@ -13,7 +13,11 @@ function About() {
 
 
             <h1> About {ar.name} {ar.contact} </h1>
-            <h1> arr value := {arr[0]}, {arr[1]}, {arr[2]}, {arr[3]}</h1>
+            <h1> arr value := {arr[0]}, {arr[1]}, {arr[2]}, {arr[3]}, {arr[4]}</h1>
+
+            <button onClick={() => setAr({name:"billo", contact:1234})}>Change object </button> <br /><br />
+
+            <button onClick={() => setarr([arr[0]=2, arr[1]=99])}>Change arr</button>
         </>
     )
 }
