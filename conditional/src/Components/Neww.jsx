bfffaada2cd108eb20ac901ffe67663d913a3b5d
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-06 18:56:57 +0530

    6 march

--- conditional/src/Components/Neww.jsx
+++ conditional/src/Components/Neww.jsx
@@ -3,6 +3,15 @@ import { useState } from "react"
 function Neww(){
 
     let [st,setSt] = useState(true)
+  
+    let [Count, setCount] = useState(0)
+   
+    function Dec(){
+
+        if(Count !=0){
+            setCount(Count-1)
+        }
+    }
     return(
         <>
        
@@ -12,6 +21,14 @@ function Neww(){
         <button onClick={()=> setSt(true)}>Show</button> <br /><br />
 
         {!st ?   <button onClick={()=> setSt(!st)}> show </button>:   <button onClick={()=> setSt(!st)}> hide </button> }
+
+       
+  <br /><br />
+
+     <h1>Count : {Count}</h1>
+      <button onClick={() => setCount(Count+1)}> + </button>
+      <button onClick={() => setCount(Dec)}> - </button>
+
 
         </>
     )
