ba8844093aad07f77d01a0a4dc62e019a9b61825
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-12 20:24:23 +0530

    12 March

--- bootstrap/src/App.jsx
+++ bootstrap/src/App.jsx
@@ -69,6 +69,26 @@ function App() {
 </Container>
 
 
+
+
+
+    <Container>
+      <Row>
+      <Col>
+      <h1>First Column</h1>
+      </Col>
+      <Col>
+      <h1>Second Column</h1>
+      </Col>
+      </Row>
+    </Container>
+    
+    
+
+
+export default App
+
+
     </>
   );
 }
