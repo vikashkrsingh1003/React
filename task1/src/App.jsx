b49f16c7fcc6bf2ae1ba401406f35a694c395221
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-14 22:07:00 +0530

    12 March

--- task1/src/App.jsx
+++ task1/src/App.jsx
@@ -96,7 +96,19 @@ function App (){
       <Card.Body>
         <Card.Title>Special title treatment</Card.Title>
         <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
+            With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+    
+    <Card>
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+            With supporting text below as a natural lead-in to additional content.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
