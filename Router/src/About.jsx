5bb44155be74e7c8ab893569e9954ba82b052ffc
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-24 18:36:18 +0530

    24 march

--- Router/src/About.jsx
+++ Router/src/About.jsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom"
+
+
+
+
 function About(){
+    let navigator = useNavigate()
+  function servicee(){
+       navigator('/Services')
+  }
     return(
         <>
-        <h1>About Page section </h1>
+        <h1>About page </h1>
+        <ul>
+        <button onClick={servicee}>Service</button>
+        </ul>
         </>
     )
 }
-
 
 export default About
