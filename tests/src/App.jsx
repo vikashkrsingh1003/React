c5dfa708d04dd60f9d1a07e583e3502f7b9d87e9
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-25 19:02:06 +0530

    test

---
+++ tests/src/App.jsx
@@ -0,0 +1,57 @@
+
+
+
+import './App.css'
+
+import Render from './Render.jsx'
+
+
+function App() {
+  return (
+
+    
+         <>
+  <nav> 
+       <div className="header">
+       <h1>LOGO</h1>
+        
+       
+            <ul>
+                <li> Home </li>
+                <li> About </li>
+
+                <li> Contact </li>
+                <li> Cart </li>
+
+                <button>Sign in</button>
+            </ul>
+        </div>
+  </nav>
+
+
+  
+<div className="left">
+          <ul>
+        <h1> Hello Everybody</h1>
+       <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia enim rerum vero! Beatae ducimus recusandae iste! Commodi aperiam, repellendus nobis explicabo molestias temporibus quia ad id asperiores eum atque eius.</p>
+
+       <button> Explore now </button>
+       </ul>
+       <ul>
+        <img src="thailand.jpg" alt="" height="500px"/>
+        </ul>
+    </div> 
+
+
+    <div> 
+
+      <button className="render "> Product List </button>
+    </div>
+ 
+
+    </>
+
+  );
+}
+
+export default App;
---
+++ tests/src/Render.jsx
@@ -0,0 +1,26 @@
+
+
+ function Render(){
+
+    return(
+
+        <>
+        
+        <div className="left">
+          <ul>
+        <h1> Hello Everybody</h1>
+       <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia enim rerum vero! Beatae ducimus recusandae iste! Commodi aperiam, repellendus nobis explicabo molestias temporibus quia ad id asperiores eum atque eius.</p>
+
+       <button> Explore now </button>
+       </ul>
+       <ul>
+        <img src="thailand.jpg" alt="" height="500px"/>
+        </ul>
+    </div> 
+
+        </>
+    )
+
+    
+ }
+ export default Render
